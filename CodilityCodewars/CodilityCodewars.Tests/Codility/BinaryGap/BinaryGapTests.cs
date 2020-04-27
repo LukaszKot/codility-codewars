6a305e54028dad92f97c74b846170c5882fb150a
LukaszKot <lukaszkot03@gmail.com>
2020-04-27 12:19:32 +0200

    Done BinaryGap.

---
+++ CodilityCodewars/CodilityCodewars.Tests/Codility/BinaryGap/BinaryGapTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodilityCodewars.Tests.Codility.BinaryGap
+{
+    [TestFixture]
+    public class BinaryGapTests
+    {
+        [Test]
+        public void given_1041_returns_5()
+        {
+            var binaryGap = new CodilityCodewars.Codility.BinaryGap.BinaryGap();
+            Assert.AreEqual(5,binaryGap.Solution(1041));
+        }
+
+        [Test]
+        public void given_9_returns_2()
+        {
+            var binaryGap = new CodilityCodewars.Codility.BinaryGap.BinaryGap();
+            Assert.AreEqual(2, binaryGap.Solution(9));
+        }
+
+        [Test]
+        public void given_529_returns_4()
+        {
+            var binaryGap = new CodilityCodewars.Codility.BinaryGap.BinaryGap();
+            Assert.AreEqual(4, binaryGap.Solution(529));
+        }
+
+        [Test]
+        public void given_20_returns_1()
+        {
+            var binaryGap = new CodilityCodewars.Codility.BinaryGap.BinaryGap();
+            Assert.AreEqual(1, binaryGap.Solution(20));
+        }
+
+        [Test]
+        public void given_15_returns_0()
+        {
+            var binaryGap = new CodilityCodewars.Codility.BinaryGap.BinaryGap();
+            Assert.AreEqual(0, binaryGap.Solution(15));
+        }
+
+        [Test]
+        public void given_561892_returns_0()
+        {
+            var binaryGap = new CodilityCodewars.Codility.BinaryGap.BinaryGap();
+            Assert.AreEqual(3, binaryGap.Solution(561892));
+        }
+    }
+}
---
+++ CodilityCodewars/CodilityCodewars/Codility/BinaryGap/BinaryGap.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CodilityCodewars.Codility.BinaryGap
+{
+    public class BinaryGap
+    {
+        public int Solution(int N)
+        {
+            var nAsString = Convert.ToString(N, 2);
+
+            var maxGapLenght = 0;
+            var gapLenght = -1;
+            var inGap = false;
+
+            for(int i=1; i<nAsString.Length; i++)
+            {
+                if(inGap)
+                {
+                    if(nAsString[i]=='0')
+                    {
+                        gapLenght++;
+                    }
+                    else
+                    {
+                        if(maxGapLenght<gapLenght)
+                        {
+                            maxGapLenght = gapLenght;
+                        }
+                        gapLenght = -1;
+                        inGap = false;
+                    }
+                }
+                else
+                {
+                    if(nAsString[i]=='0' && nAsString[i-1]=='1')
+                    {
+                        inGap = true;
+                        gapLenght = 1;
+                    }
+                }
+            }
+
+            return maxGapLenght;
+        }
+    }
+}
