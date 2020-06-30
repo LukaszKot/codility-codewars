0fc3b1ad25650603b0e3c4708cbd64833b8708f2
LukaszKot <lukaszkot03@gmail.com>
2020-06-30 13:47:51 +0200

    Done MaxCounters.

---
+++ CodilityCodewars/CodilityCodewars.Tests/Codility/MaxCountersTests.cs
@@ -0,0 +1,15 @@
+﻿using CodilityCodewars.Codility;
+using NUnit.Framework;
+
+namespace CodilityCodewars.Tests.Codility
+{
+    [TestFixture]
+    public class MaxCountersTests
+    {
+        [TestCase(5,new int[] { 3,4,4,6,1,4,4 }, ExpectedResult = new int[] { 3, 2, 2, 4, 2 })]
+        public int[] Test(int N, int[] A)
+        {
+            return new MaxCounters().Solution(N, A);
+        }
+    }
+}
---
+++ CodilityCodewars/CodilityCodewars/Codility/MaxCounters.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace CodilityCodewars.Codility
+{
+    public class MaxCounters
+    {
+        public int[] Solution(int N, int[] A)
+        {
+            int[] counters = Enumerable.Repeat(0, N).ToArray();
+            int currentMax = 0;
+            int lastUpdate = 0;
+            foreach(var operation in A)
+            {
+                if(operation==N+1)
+                {
+                    lastUpdate = currentMax;
+                }
+                else
+                {
+                    if(counters[operation - 1]>lastUpdate)
+                    {
+                        counters[operation - 1]++;
+                    }
+                    else
+                    {
+                        counters[operation - 1] = lastUpdate + 1;
+                    }
+                    
+                    if (currentMax < counters[operation - 1]) currentMax = counters[operation - 1];
+                }
+            }
+            return counters.Select(x => {
+                if (x > lastUpdate) return x;
+                else return lastUpdate;
+            }).ToArray();
+        }
+    }
+}
