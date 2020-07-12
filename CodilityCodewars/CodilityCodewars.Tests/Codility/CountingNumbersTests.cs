4cde5ee7a2e4e92b460f7c5939f3ed83913f98cd
LukaszKot <lukaszkot03@gmail.com>
2020-07-12 19:29:24 +0200

    Done CountingNumbers.

---
+++ CodilityCodewars/CodilityCodewars.Tests/Codility/CountingNumbersTests.cs
@@ -0,0 +1,17 @@
+﻿using CodilityCodewars.Codility;
+using NUnit.Framework;
+
+namespace CodilityCodewars.Tests.Codility
+{
+    [TestFixture]
+    public class CountingNumbersTests
+    {
+        [TestCase(new int[] { 1,3,6,4,1,2 }, ExpectedResult = 5)]
+        [TestCase(new int[] { 1, 2, 3 }, ExpectedResult = 4)]
+        [TestCase(new int[] { -1, -3 }, ExpectedResult = 1)]
+        public int test(int[] A)
+        {
+            return new CountingNumbers().Solution(A);
+        }
+    }
+}
---
+++ CodilityCodewars/CodilityCodewars/Codility/CountingNumbers.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace CodilityCodewars.Codility
+{
+    public class CountingNumbers
+    {
+        public int Solution(int[] A)
+        {
+            return Enumerable.Range(1,1000000).Except(A).First();
+        }
+    }
+}
