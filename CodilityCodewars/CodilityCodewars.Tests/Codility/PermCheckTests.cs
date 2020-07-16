2d7bd57a7bc41ff7e74def7ba9d08110e47e0f4b
LukaszKot <lukaszkot03@gmail.com>
2020-07-16 14:56:02 +0200

    Done PermCheck.

---
+++ CodilityCodewars/CodilityCodewars.Tests/Codility/PermCheckTests.cs
@@ -0,0 +1,16 @@
+﻿using CodilityCodewars.Codility;
+using NUnit.Framework;
+
+namespace CodilityCodewars.Tests.Codility
+{
+    [TestFixture]
+    public class PermCheckTests
+    {
+        [TestCase(new int[] { 4,1,3,2 }, ExpectedResult = 1)]
+        [TestCase(new int[] { 4, 1, 3 }, ExpectedResult = 0)]
+        public int Test(int[] A)
+        {
+            return new PermCheck().Solution(A);
+        }
+    }
+}
---
+++ CodilityCodewars/CodilityCodewars/Codility/PermCheck.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace CodilityCodewars.Codility
+{
+    public class PermCheck
+    {
+        public int Solution(int[] A)
+        {
+            int check = 1;
+            foreach(var el in A.OrderBy(x => x))
+            {
+                if (check != el) return 0;
+                check++;
+            }
+            return 1;
+        }
+    }
+}
