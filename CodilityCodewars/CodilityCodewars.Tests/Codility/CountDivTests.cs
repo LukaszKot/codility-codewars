145e825cc1fa0a4f72fcc9f2146ad95071e19d31
LukaszKot <lukaszkot03@gmail.com>
2020-07-22 13:23:17 +0200

    Done CountDiv.

---
+++ CodilityCodewars/CodilityCodewars.Tests/Codility/CountDivTests.cs
@@ -0,0 +1,23 @@
+﻿using CodilityCodewars.Codility;
+using NUnit.Framework;
+
+namespace CodilityCodewars.Tests.Codility
+{
+    [TestFixture]
+    public class CountDivTests
+    {
+        [TestCase(6,11,2, ExpectedResult = 3)]
+        [TestCase(6, 11, 3, ExpectedResult = 2)]
+        [TestCase(6, 11, 4, ExpectedResult = 1)]
+        [TestCase(4, 12, 4, ExpectedResult = 3)]
+        [TestCase(1000001,120000000,1000000, ExpectedResult= 119)]
+        [TestCase(1000000, 119999999, 1000000, ExpectedResult = 119)]
+        [TestCase(1000000, 1200000000, 1000000, ExpectedResult = 1200)]
+        [TestCase(1000000, 1200000000, 10000000, ExpectedResult = 120)]
+        [TestCase(int.MaxValue, int.MaxValue, int.MaxValue, ExpectedResult = 1)]
+        public int Tests(int A, int B, int K)
+        {
+            return new CountDiv().Solution(A, B, K);
+        }
+    }
+}
---
+++ CodilityCodewars/CodilityCodewars/Codility/CountDiv.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CodilityCodewars.Codility
+{
+    public class CountDiv
+    {
+        public int Solution(int A, int B, int K)
+        {
+            var firstSearchedNumber = 0;
+            for(long i=A; i<=((long)A+(long)K);i++)
+            {
+                if (i % K == 0)
+                {
+                    firstSearchedNumber = (int)i;
+                    break;
+                }
+            }
+
+            return (int)Math.Floor(((double)B -(double)firstSearchedNumber) / (double)K)+1;
+        }
+    }
+}
