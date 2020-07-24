d2f9fd9255f98486578a0c6a35bc8964b0055f39
LukaszKot <lukaszkot03@gmail.com>
2020-07-24 14:53:37 +0200

    Done GenomicRangeQuery.

---
+++ CodilityCodewars/CodilityCodewars.Tests/Codility/GenomicRangeQueryTests.cs
@@ -0,0 +1,15 @@
+﻿using CodilityCodewars.Codility;
+using NUnit.Framework;
+
+namespace CodilityCodewars.Tests.Codility
+{
+    [TestFixture]
+    public class GenomicRangeQueryTests
+    {
+        [TestCase("CAGCCTA", new int[] { 2,5,0 }, new int[] { 4,5,6 }, ExpectedResult = new int[] { 2,4,1 })]
+        public int[] Test(string S, int[] P, int[] Q)
+        {
+            return new GenomicRangeQuery().Solution(S, P, Q);
+        }
+    }
+}
---
+++ CodilityCodewars/CodilityCodewars/Codility/GenomicRangeQuery.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CodilityCodewars.Codility
+{
+    public class GenomicRangeQuery
+    {
+        private IDictionary<char,int> dictionary = new Dictionary<char,int>
+        {
+            ['A'] = 1,
+            ['C'] = 2,
+            ['G'] = 3,
+            ['T'] = 4
+        };
+
+        public int[] Solution(string S, int[] P, int[] Q)
+        {
+            var prefixSums = new int[S.Length, 4];
+            for(var i=0; i<S.Length; i++)
+            {
+                if(i==0)
+                {
+                    prefixSums[i, dictionary[S[i]] - 1] = dictionary[S[i]];
+                }
+                else
+                {
+                    for(var j=0; j<4; j++)
+                    {
+                        prefixSums[i, j] += prefixSums[i - 1, j];
+                    }
+
+                    prefixSums[i, dictionary[S[i]] - 1] += dictionary[S[i]];
+                }
+            }
+
+            int[] result = new int[Q.Length];
+            for (var i = 0; i < P.Length; i++)
+            {
+                foreach(var key in dictionary.Keys)
+                {
+                    if(prefixSums[Q[i],dictionary[key]-1] - (P[i]-1<0 ? 0 :  prefixSums[P[i]-1, dictionary[key] - 1]) > 0)
+                    {
+                        result[i] = dictionary[key];
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+    }
+}
