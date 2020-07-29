712d3b95f344ddf8df8956541812e806978985ea
LukaszKot <lukaszkot03@gmail.com>
2020-07-29 17:16:30 +0200

    Done MinAvgTwoSlice.

---
+++ CodilityCodewars/CodilityCodewars.Tests/Codility/MinAvgTwoSliceTests.cs
@@ -0,0 +1,18 @@
+﻿using CodilityCodewars.Codility;
+using NUnit.Framework;
+using System.Linq;
+
+namespace CodilityCodewars.Tests.Codility
+{
+    [TestFixture]
+    public class MinAvgTwoSliceTests
+    {
+        [TestCase(new int[] { 4,2,2,5,1,5,8  }, ExpectedResult = 1)]
+        [TestCase(new int[] { int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue }, ExpectedResult = 0)]
+        [TestCase(new int[] { 10000, 10000, 10000, 10000, 10000, 10000, 10000 }, ExpectedResult = 0)]
+        public int Test(int[] A)
+        {
+            return new MinAvgTwoSlice().Solution(A);
+        }
+    }
+}
---
+++ CodilityCodewars/CodilityCodewars/Codility/MinAvgTwoSlice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CodilityCodewars.Codility
+{
+    public class MinAvgTwoSlice
+    {
+        public int Solution(int[] A)
+        {
+            long[] prefixSums = new long[A.Length];
+            for(var i=0; i<A.Length; i++)
+            {
+                if(i==0)
+                {
+                    prefixSums[0] = A[0];
+                }
+                else
+                {
+                    prefixSums[i] = prefixSums[i - 1] + A[i];
+                }
+            }
+            double lowestAverage = long.MaxValue;
+            var lowestQ = A.Length;
+            for (var i=0; i<A.Length-1; i++)
+            {
+                for(var j=i+1; j<(A.Length<i+3? A.Length : i+3); j++)
+                {
+                    double average = ((double)prefixSums[j] - (double)(i==0 ? 0 : prefixSums[i - 1]))/(double)(j-i+1);
+                    if(average<lowestAverage)
+                    {
+                        lowestAverage = average;
+                        lowestQ = i;
+                    }
+                }
+            }
+            return lowestQ;
+        }
+    }
+}
