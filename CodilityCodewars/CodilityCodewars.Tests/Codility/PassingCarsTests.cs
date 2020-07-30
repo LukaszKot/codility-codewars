834edd9b702bc873adc818538fd76295a04b3ee8
LukaszKot <lukaszkot03@gmail.com>
2020-07-30 15:02:45 +0200

    Done passing cars.

---
+++ CodilityCodewars/CodilityCodewars.Tests/Codility/PassingCarsTests.cs
@@ -0,0 +1,15 @@
+﻿using CodilityCodewars.Codility;
+using NUnit.Framework;
+
+namespace CodilityCodewars.Tests.Codility
+{
+    [TestFixture]
+    public class PassingCarsTests
+    {
+        [TestCase(new int[] { 0, 1, 0, 1, 1 }, ExpectedResult = 5)]
+        public int Test(int[] A)
+        {
+            return new PassingCars().Solution(A);
+        }
+    }
+}
---
+++ CodilityCodewars/CodilityCodewars/Codility/PassingCars.cs
@@ -0,0 +1,29 @@
+﻿namespace CodilityCodewars.Codility
+{
+    public class PassingCars
+    {
+        public int Solution(int[] A)
+        {
+            var prefixSum = new int[A.Length];
+            for(var i=0; i<prefixSum.Length; i++)
+            { 
+                if(i > 0)
+                {
+                    prefixSum[i] = prefixSum[i - 1];
+                }
+                if(A[i]==1) prefixSum[i]++;
+            }
+
+            var result = 0;
+            for(var i=0; i<A.Length; i++)
+            {
+                if(A[i]==0)
+                {
+                    result += prefixSum[A.Length - 1] - prefixSum[i];
+                    if (result > 1000000000) return -1;
+                }
+            }
+            return result;
+        }
+    }
+}
