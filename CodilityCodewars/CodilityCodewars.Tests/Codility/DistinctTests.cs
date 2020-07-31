60c4977db8c635de4d9ec59720d3fba5aa5e8a09
LukaszKot <lukaszkot03@gmail.com>
2020-07-31 11:25:13 +0200

    Done Distinct.

---
+++ CodilityCodewars/CodilityCodewars.Tests/Codility/DistinctTests.cs
@@ -0,0 +1,15 @@
+﻿using CodilityCodewars.Codility;
+using NUnit.Framework;
+
+namespace CodilityCodewars.Tests.Codility
+{
+    [TestFixture]
+    public class DistinctTests
+    {
+        [TestCase(new int[] { 2,1,1,2,3,1 }, ExpectedResult = 3)]
+        public int Test(int[] A)
+        {
+            return new Distinct().Solution(A);
+        }
+    }
+}
---
+++ CodilityCodewars/CodilityCodewars/Codility/Distinct.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace CodilityCodewars.Codility
+{
+    public class Distinct
+    {
+        public int Solution(int[] A)
+        {
+            return A.Distinct().Count();
+        }
+    }
+}
