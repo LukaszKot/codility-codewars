f1d0f92b08667b80508d9f546dad6740e4a8163f
LukaszKot <lukaszkot03@gmail.com>
2020-08-03 12:51:05 +0200

    Done MaxProductOfThree.

---
+++ CodilityCodewars/CodilityCodewars.Tests/Codility/MaxProductOfThreeTests.cs
@@ -0,0 +1,16 @@
+﻿using CodilityCodewars.Codility;
+using NUnit.Framework;
+
+namespace CodilityCodewars.Tests.Codility
+{
+    [TestFixture]
+    public class MaxProductOfThreeTests
+    {
+        [TestCase(new int[] { -3, 1, 2, -2, 5, 6 }, ExpectedResult = 60)]
+        [TestCase(new int[] { -5, 5, -5, 4}, ExpectedResult = 125)]
+        public int Test(int[] A)
+        {
+            return new MaxProductOfThree().Solution(A);
+        }
+    }
+}
---
+++ CodilityCodewars/CodilityCodewars/Codility/MaxProductOfThree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace CodilityCodewars.Codility
+{
+    public class MaxProductOfThree
+    {
+        public int Solution(int[] A)
+        {
+            Array.Sort(A);
+            int first;
+            int second = 1;
+
+            foreach (var n in A.Skip(A.Length - 3))
+            {
+                second *= n;
+            }
+
+            if (A[0]*A[1]>0)
+            {
+                first = A[0] * A[1] * A[A.Length - 1];
+                if (first > second) return first;
+                return second;
+            }
+
+            
+            return second;
+            
+        }
+    }
+}
